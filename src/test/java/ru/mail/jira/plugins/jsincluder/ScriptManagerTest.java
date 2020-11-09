4185f647b9ebd1575678904ba41d95755450a597
Ivan Mashintsev <i.mashintsev@corp.mail.ru>
2020-11-09 13:55:44 +0300

    test reports

---
+++ src/test/java/ru/mail/jira/plugins/jsincluder/ScriptManagerTest.java
@@ -0,0 +1,48 @@
+package ru.mail.jira.plugins.jsincluder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ScriptManagerTest {
+
+  @Test
+  void getScript() {
+  }
+
+  @Test
+  void getScripts() {
+  }
+
+  @Test
+  void getBinding() {
+  }
+
+  @Test
+  void findBindings() {
+  }
+
+  @Test
+  void createScript() {
+  }
+
+  @Test
+  void updateScript() {
+  }
+
+  @Test
+  void deleteScript() {
+  }
+
+  @Test
+  void createBinding() {
+  }
+
+  @Test
+  void updateBinding() {
+  }
+
+  @Test
+  void deleteBinding() {
+  }
+}
