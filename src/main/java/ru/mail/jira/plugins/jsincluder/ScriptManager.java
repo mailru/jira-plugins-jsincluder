41df52ed7c068202838c4a0731c0850a60a38dbf
Udalov Nikita <n.udalov@corp.mail.ru>
2020-11-18 09:56:45 +0300

    Changed == 1 to Boolean.TRUE param

--- src/main/java/ru/mail/jira/plugins/jsincluder/ScriptManager.java
+++ src/main/java/ru/mail/jira/plugins/jsincluder/ScriptManager.java
@@ -62,20 +62,24 @@ public class ScriptManager {
             String whereClause = "(PROJECT_ID = ? OR PROJECT_ID IS NULL)";
             switch (context) {
               case CREATE:
-                whereClause += " AND CREATE_CONTEXT_ENABLED = 1";
+                whereClause += " AND CREATE_CONTEXT_ENABLED = ?";
                 break;
               case VIEW:
                 whereClause +=
-                    " AND (VIEW_CONTEXT_ENABLED = 1 OR EDIT_CONTEXT_ENABLED = 1 OR TRANSITION_CONTEXT_ENABLED = 1)";
-                break;
+                    " AND (VIEW_CONTEXT_ENABLED = ? OR EDIT_CONTEXT_ENABLED = ? OR TRANSITION_CONTEXT_ENABLED = ?)";
+                return ao.find(
+                    Binding.class,
+                    Query.select()
+                        .where(whereClause, projectId, Boolean.TRUE, Boolean.TRUE, Boolean.TRUE));
               case EDIT:
-                whereClause += " AND EDIT_CONTEXT_ENABLED = 1";
+                whereClause += " AND EDIT_CONTEXT_ENABLED = ?";
                 break;
               case TRANSITION:
-                whereClause += " AND TRANSITION_CONTEXT_ENABLED = 1";
+                whereClause += " AND TRANSITION_CONTEXT_ENABLED = ?";
                 break;
             }
-            return ao.find(Binding.class, Query.select().where(whereClause, projectId));
+            return ao.find(
+                Binding.class, Query.select().where(whereClause, projectId, Boolean.TRUE));
           }
         });
   }
